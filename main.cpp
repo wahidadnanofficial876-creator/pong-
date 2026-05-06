84da15f8f426293b4b9e6398a68de9fb2f12f396
wahidadnanofficial876-creator <wahidadnanofficial876@gmail.com>
2026-05-07 00:32:24 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,43 +1,148 @@
 #include <iostream>
 #include <raylib.h>
-
 using namespace std;
 
-int main () {
+int screenWidth=800;
+int screenHieght=600;
+class paddle;	
+class ball
+{
+	private:
+		int ballCenterX;
+		int ballCenterY;
+		int ballRadius;
+		int ballSpeedX;
+		int ballSpeedY;
+		Color ballColor;
+		public:
+			ball(int x,int y,int r,int ballSpeedX,int ballSpeedY,Color color)
+			{
+				ballCenterX=x;
+				ballCenterY=y;
+				ballRadius=r;
+				ballColor=color;
+				this->ballSpeedX=ballSpeedX;
+				this->ballSpeedY=ballSpeedY;
+			}
+			void drawBall()
+			{
+			DrawCircle(ballCenterX,ballCenterY,ballRadius,WHITE);	
+			}
+			void updateBallpos()
+			{
+				ballCenterX=ballCenterX+ballSpeedX;
+				ballCenterY=ballCenterY+ballSpeedY;
+				
+				if(ballCenterX+ballRadius>=screenWidth or ballCenterX-ballRadius<=0)
+				{
+					ballSpeedX*=-1;
+				}
+				if(ballCenterY+ballRadius>=screenHieght or ballCenterY-ballRadius<=0)
+				{
+					ballSpeedY*=-1;
+				}
+				
+			}
+			friend void checkCollision(ball &b,paddle &p);
+};
 
-    const int SCREEN_WIDTH = 800;
-    const int SCREEN_HEIGHT = 600;
-    int ball_x = 100;
-    int ball_y = 100;
-    int ball_speed_x = 5;
-    int ball_speed_y = 5;
-    int ball_radius = 15;
+class paddle
+{
+	private:
+		int paddleX;
+		int paddleY;
+		int paddleHieght;
+		int paddleWidth;
+		int paddleSpeedY;
+		Color paddleColor;
+		
+		public:
+			paddle(int px,int py,int paddleHieght,int paddleWidth,Color color)
+			{
+				paddleX=px;
+				paddleY=py;
+				this->paddleHieght=paddleHieght;
+				this->paddleWidth=paddleWidth;
+				paddleColor=color;
+				
+			}
+			
+			void drawPaddle()
+			{
+				DrawRectangle(paddleX,paddleY,paddleWidth,paddleHieght,WHITE);
+			}
+			void movePaddle()
+			{
+				paddleSpeedY=5;
+				if(IsKeyDown(KEY_UP))
+				{	
+				paddleY-=paddleSpeedY;
+						
+				}
+				 if(IsKeyDown(KEY_DOWN))
+				{
+												
+		          paddleY+=paddleSpeedY;
+				  							
+				}
+				else{
+					paddleSpeedY=0;
+				}
+				
+			}
+			void stopPaddle()
+			{
+				if(paddleY<=0)
+				{
+					paddleY=0;
+				}
+				if(paddleY>=screenHieght-paddleHieght)
+				{
+					paddleY=screenHieght-paddleHieght;
+				}
+				
+			}
+friend void checkCollision(ball &b , paddle &p);			
+};
 
-    cout << "Hello World" << endl;
+int hits=0;
+void checkCollision(ball &b ,paddle &p)
+{
+	 
+	if((b.ballCenterX)-(b.ballRadius)<=p.paddleX+p.paddleWidth && b.ballCenterY>=p.paddleY && b.ballCenterY<p.paddleY+p.paddleHieght)
+	{
+		b.ballSpeedX*=-1;
+		hits++;
+	}
+	
+}
 
-    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "My first RAYLIB program!");
-    SetTargetFPS(60);
+int main()
+{
+SetTargetFPS(60); 
+ball b1(400,300,30,5,5,WHITE);
 
-    while (WindowShouldClose() == false){
-   
-        ball_x += ball_speed_x;
-        ball_y += ball_speed_y;
 
-        if(ball_x + ball_radius >= SCREEN_WIDTH || ball_x - ball_radius <= 0)
-        {
-            ball_speed_x *= -1;
-        }
+paddle p1(30, (screenHieght - 125)/2, 125, 25, WHITE); 
+paddle p2(screenWidth - 30 - 25, (screenHieght - 125) / 2, 125, 25,WHITE);
 
-        if(ball_y + ball_radius >= SCREEN_HEIGHT || ball_y - ball_radius <= 0)
-        {
-            ball_speed_y *= -1;
-        }
-        
-        BeginDrawing();
-            ClearBackground(BLACK);
-            DrawCircle(ball_x,ball_y,ball_radius, WHITE);
-        EndDrawing();
-    }
 
-    CloseWindow();
+	InitWindow(screenWidth,screenHieght,"My first Game");
+	
+	
+	while(!WindowShouldClose())
+	{			
+		BeginDrawing();
+		ClearBackground(BLACK);
+		b1.drawBall();
+		b1.updateBallpos();
+		checkCollision(b1, p1);
+		p1.drawPaddle();
+		p1.movePaddle();
+		p1.stopPaddle();
+		p2.drawPaddle();
+		DrawText(("Hits: " + std::to_string(hits)).c_str() , 20 , 20,20 , WHITE);
+		EndDrawing();
+	}
+	CloseWindow();
 }
